2232fbfe54c6f004abac07cd366246b831b07cbd
SMG <scandanavianmountaingoat@gmail.com>
2020-08-09 22:04:49 -0500

    use database for graph vector4

--- Scripts/graph_vector4.py
+++ Scripts/graph_vector4.py
@@ -3,39 +3,50 @@ import os
 import matplotlib.pyplot as plt 
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np 
-import ast
+import sqlite3
 
-if len(sys.argv) < 2:
-    print('Usage:')
-    print(f'\t{sys.argv[0]} <unique value dump file>')
-    exit(1)
-    
-values = []
-with open(sys.argv[1]) as f:
-    for line in f.readlines():
-        # Workaround for tuples having spaces between values
-        # Vectors will be read as tuples of floats
-        value = line[line.index(' ')+1:line.index('\\')]
-        values.append(ast.literal_eval(value))
-values = np.array(values)
+select_param_id_sql = 'SELECT Id FROM CustomParam WHERE Name = ?'
+select_values_sql = 'SELECT DISTINCT Value1, Value2, Value3, Value4 FROM CustomVectorParam WHERE ParamID = ?'
 
-# Use the XYZ values as colors.
-colors = np.clip(values[:,:3],0,1)
+def get_values(db_file, param_name):
+    with sqlite3.connect(db_file) as con:
+        param_id = con.execute(select_param_id_sql, (param_name,)).fetchone()
+        if param_id is None:
+            print(f'Could not find the Param ID for {param_name}')
+            exit(1)
 
-# Plot the values in 3d.
-fig = plt.figure()
-ax = Axes3D(fig)
+        values = con.execute(select_values_sql, param_id).fetchall()
+        return np.array(values)
 
-# Chart title
-ax.text2D(0.05, 0.95, os.path.basename(sys.argv[1]), transform=ax.transAxes) 
 
-# Ignore colors that are too bright.
-# ax.set_xlim(0, 1)
-# ax.set_ylim(0, 1)
-# ax.set_zlim(0, 1)
-ax.set_xlabel('Red')
-ax.set_ylabel('Green')
-ax.set_zlabel('Blue')
-ax.scatter(values[:,0],values[:,1],values[:,2],c=colors,edgecolor='k') 
+def graph_values(param_name, values):
+    # Use the XYZ values as colors.
+    colors = np.clip(values[:,:3],0,1)
 
-plt.show()
+    # Plot the values in 3d.
+    fig = plt.figure()
+    ax = Axes3D(fig)
+
+    # Chart title
+    ax.text2D(0.05, 0.95, param_name, transform=ax.transAxes) 
+
+    ax.set_xlabel('Red (X)')
+    ax.set_ylabel('Green (Y)')
+    ax.set_zlabel('Blue (Z)')
+    ax.scatter(values[:,0],values[:,1],values[:,2],c=colors,edgecolor='k') 
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print('Usage:')
+        print(f'\t{sys.argv[0]} <Smush Materials SQLite DB> <CustomVector Param>')
+        exit(1)
+        
+    db_file = sys.argv[1]
+    param_name = sys.argv[2]
+
+    values = get_values(db_file, param_name)
+    graph_values(param_name, values)
+
