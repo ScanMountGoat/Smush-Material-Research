2aeea0b89b8fdda31db0fe1d423dc0d1c9e0abc7
SMG <scandanavianmountaingoat@gmail.com>
2021-09-01 19:13:54 -0500

    update the scripts to use the new xmb.exe instead of XMBDec.py

--- Scripts/batch_export_xmb_to_xml.py
+++ Scripts/batch_export_xmb_to_xml.py
@@ -4,11 +4,11 @@ import subprocess
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
-        print(f'Usage: python {sys.argv[0]} <XMBDec.py> <XMB source folder> <destination folder>')
-        print(f'example: python {sys.argv[0]} XMBDec.py "Xmb Inputs" "XML Output"')
+        print(f'Usage: python {sys.argv[0]} <xmb.exe> <XMB source folder> <destination folder>')
+        print(f'example: python {sys.argv[0]} xmb.exe "Xmb Inputs" "XML Output"')
         exit(1)
 
-    script = os.path.abspath(sys.argv[1])
+    xmb_exe_path = os.path.abspath(sys.argv[1])
     source_folder = os.path.abspath(sys.argv[2])
     destination_folder = os.path.abspath(sys.argv[3])
     if not os.path.exists(destination_folder):
@@ -23,5 +23,5 @@ if __name__ == '__main__':
                 output_filename = abs_path.replace(source_folder, "").replace(os.path.sep, "_").replace('.xmb', '.xml')[1:]
                 output_path = os.path.join(destination_folder, output_filename)
                
-                # Run the python script to convert.
-                subprocess.Popen([sys.executable, script, abs_path, '-o', output_path])
+                # Run the xmb executable to convert.
+                subprocess.Popen([xmb_exe_path, abs_path, output_path])
