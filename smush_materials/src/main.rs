361f0047d4b39351cfaf5b067713401797afcc82
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2022-07-26 11:57:03 -0500

    rework shader_info to generate ssbh_wgpu JSON

--- smush_materials/src/main.rs
+++ smush_materials/src/main.rs
@@ -6,6 +6,7 @@ use std::{
 
 use clap::{Arg, Command};
 use rayon::prelude::*;
+use serde::Serialize;
 use ssbh_data::SsbhData;
 use xmb_lib::XmbFile;
 use xmltree::EmitterConfig;
@@ -40,15 +41,28 @@ fn main() {
         )
         .subcommand(
             Command::new("shader_info")
-                .about("Export shader input information")
+                .about("Export shader info JSON")
                 .arg(
-                    Arg::new("input")
+                    Arg::new("nufxlb")
                         .index(1)
                         .help("The input nufxlb file")
                         .required(true)
                         .takes_value(true),
                 )
-                .arg(output_arg.clone()),
+                .arg(
+                    Arg::new("decompiled_shaders")
+                        .index(2)
+                        .help("The folder of decompiled GLSL shaders")
+                        .required(true)
+                        .takes_value(true),
+                )
+                .arg(
+                    Arg::new("output")
+                        .index(3)
+                        .help("The output shader info JSON")
+                        .required(true)
+                        .takes_value(true),
+                ),
         )
         .subcommand(
             Command::new("shader_binaries")
@@ -84,7 +98,8 @@ fn main() {
             shdrs_to_bin,
         ),
         ("shader_info", sub_m) => export_nufxlb_shader_info(
-            sub_m.value_of("input").unwrap(),
+            sub_m.value_of("nufxlb").unwrap(),
+            sub_m.value_of("decompiled_shaders").unwrap(),
             sub_m.value_of("output").unwrap(),
         ),
         _ => 0,
@@ -93,56 +108,72 @@ fn main() {
     println!("Converted {:?} files in {:?}", count, start.elapsed());
 }
 
-fn export_nufxlb_shader_info(input_file: &str, destination_folder: &str) -> usize {
-    // Make sure the output directory exists.
-    if !Path::new(destination_folder).exists() {
-        std::fs::create_dir(destination_folder).unwrap();
-    }
+#[derive(Debug, Serialize)]
+struct ShaderDatabase {
+    shaders: Vec<ShaderProgram>,
+}
 
-    match ssbh_lib::formats::nufx::Nufx::from_file(input_file) {
-        Ok(ssbh_lib::formats::nufx::Nufx::V0(nufx)) => {
-            for program in nufx.programs.elements {
-                println!("{}", program.name.to_string_lossy());
-                let parameters = program
-                    .material_parameters
-                    .elements
-                    .iter()
-                    .map(|p| p.parameter_name.to_string_lossy())
-                    .collect::<Vec<String>>()
-                    .join("\n");
-                println!("=== Material Parameters ===\n{}", parameters);
-            }
-        }
+#[derive(Debug, Serialize)]
+struct ShaderProgram {
+    name: String,
+    discard: bool,
+    attrs: Vec<String>,
+    params: Vec<String>,
+}
+
+fn export_nufxlb_shader_info(nufx_file: &str, shader_folder: &str, output_file: &str) -> usize {
+    // Generate the shader info JSON for ssbh_wgpu.
+    match ssbh_lib::formats::nufx::Nufx::from_file(nufx_file) {
         Ok(ssbh_lib::formats::nufx::Nufx::V1(nufx)) => {
-            for program in nufx.programs.elements {
-                let parameters = program
-                    .material_parameters
+            // TODO: Make excluding duplicate render pass entries optional?
+            let database = ShaderDatabase {
+                shaders: nufx
+                    .programs
                     .elements
-                    .iter()
-                    .map(|p| p.parameter_name.to_string_lossy())
-                    .collect::<Vec<String>>()
-                    .join("\n");
-                let attributes = program
-                    .vertex_attributes
-                    .elements
-                    .iter()
-                    .map(|a| a.attribute_name.to_string_lossy())
-                    .collect::<Vec<String>>()
-                    .join("\n");
+                    .into_iter()
+                    .filter(|program| program.render_pass.to_str() == Some("nu::Final"))
+                    .map(|program| {
+                        // We can infer information from the shader source using some basic heurstics.
+                        let pixel_shader = program.shaders.pixel_shader.to_string_lossy();
+                        let pixel_shader_file = Path::new(shader_folder)
+                            .join(pixel_shader)
+                            .with_extension("glsl");
+                        let pixel_source = std::fs::read_to_string(&pixel_shader_file);
 
-                let output_path = Path::new(destination_folder)
-                    .join(program.name.to_string_lossy() + "_info")
-                    .with_extension("txt");
-                let mut writer = std::fs::File::create(output_path).unwrap();
-                writeln!(
-                    &mut writer,
-                    "=== Material Parameters ===\n{}\n\n=== Vertex Attributes ===\n{}",
-                    parameters, attributes
-                )
-                .unwrap();
-            }
+                        // Alpha testing in Smash Ultimate is done in shader, so check for discard.
+                        // There may be false positives if the discard code path is unused.
+                        let discard = pixel_source
+                            .map(|source| source.contains("discard;"))
+                            .unwrap_or_default();
+
+                        // TODO: Check what color channels are used from textures (requires nushdb).
+
+                        ShaderProgram {
+                            name: program.name.to_string_lossy(),
+                            discard,
+                            attrs: program
+                                .vertex_attributes
+                                .elements
+                                .iter()
+                                .map(|a| a.attribute_name.to_string_lossy())
+                                .collect(),
+                            params: program
+                                .material_parameters
+                                .elements
+                                .iter()
+                                .map(|p| p.parameter_name.to_string_lossy())
+                                .collect(),
+                        }
+                    })
+                    .collect(),
+            };
+
+            // TODO: Make pretty printing optional.
+            let json = serde_json::to_string_pretty(&database).unwrap();
+            std::fs::write(output_file, json).unwrap();
         }
-        Err(e) => eprintln!("Error reading {:?}: {:?}", input_file, e),
+        Ok(_) => eprintln!("Only NUFX version 1.1 is supported"),
+        Err(e) => eprintln!("Error reading {:?}: {:?}", nufx_file, e),
     }
     0
 }
@@ -214,6 +245,8 @@ fn anim_data_to_json(path: &Path, output_full_path: PathBuf) {
 fn shdrs_to_bin(path: &Path, output: PathBuf) {
     match ssbh_lib::formats::shdr::Shdr::from_file(path) {
         Ok(ssbh_lib::formats::shdr::Shdr::V12 { shaders }) => {
+            // TODO: Add a flag to just output the code portion?
+            // This allows using an unmodified version of ryujinx shadertools.
             for shader in shaders.elements {
                 let output = output
                     .with_file_name(shader.name.to_string_lossy())
