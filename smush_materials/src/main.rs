040b0bab7337c2fec51d8a22ad76ad6ec8432985
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2022-12-18 17:31:03 -0600

    add a heuristic for detecting premultiplied alpha

--- smush_materials/src/main.rs
+++ smush_materials/src/main.rs
@@ -125,6 +125,7 @@ struct ShaderDatabase {
 struct ShaderProgram {
     name: String,
     discard: bool,
+    premultiplied: bool,
     attrs: Vec<String>,
     params: Vec<String>,
     complexity: f64,
@@ -163,6 +164,11 @@ fn export_shader_info(
                             .map(|source| source.contains("discard;"))
                             .unwrap_or_default();
 
+                        let premultiplied = pixel_source
+                            .as_ref()
+                            .map(|source| is_premultiplied_alpha(source).unwrap_or_default())
+                            .unwrap_or_default();
+
                         let pixel_binary_data = shader_binary_data(binary_folder, pixel_shader);
                         let vertex_binary_data = shader_binary_data(binary_folder, vertex_shader);
 
@@ -185,6 +191,7 @@ fn export_shader_info(
                         ShaderProgram {
                             name: program.name.to_string_lossy(),
                             discard,
+                            premultiplied,
                             attrs,
                             params,
                             complexity: lines_of_code as f64,
@@ -434,4 +441,146 @@ fn flattened_output_path(
     Path::new(destination_folder)
         .join(output_file)
         .with_extension(extension)
+}
+
+fn is_premultiplied_alpha(source: &str) -> Option<bool> {
+    // Identical shader code may have different variable names or whitespace.
+    // This is known in the literature as a "type-2 code clone".
+    // A proper solution would perform a structual match with a reference.
+    // This is possible using an AST graph with normalized identifiers.
+    // Replacing all variables with "var" allows for different variable names.
+    // The argument edges should be labeled so a*b and b*a are equivalent.
+    // The code matches the premultiplied reference if the graphs are isomorphic.
+
+    // Use a simple heuristic for now based on known premultiplied shaders.
+    // This basically hardcodes the graph traversal and isomorphism check.
+    // Each assignment or input to an operation is an edge in the graph.
+    // Checking all the shaders manually to validate this is infeasible.
+    // TODO: Is it worth trying to implement a better heuristic?
+
+    // Find the variable used to set the alpha output.
+    // Assume the relevant code is in the last lines.
+    let alpha_var = most_recent_assignment(source, "out_attr0.w")?;
+
+    // The multiplied alpha should be used to initialize the var above.
+    // Find the variable assigned to the alpha output.
+    let alpha_assignment = most_recent_assignment(source, alpha_var)?;
+
+    // Find the variable used for the premultiplied alpha.
+    let multiplied_alpha_var = alpha_assignment.get(
+        alpha_assignment.chars().position(|c| c == '(')? + 1
+            ..alpha_assignment.chars().position(|c| c == ',')?,
+    )?;
+
+    // Find the variables assigned to the RGB outputs.
+    // TODO: This doesn't correctly handle the BGRA condition.
+    let var_r = most_recent_assignment(source, "out_attr0.x")?;
+    let var_g = most_recent_assignment(source, "out_attr0.y")?;
+    let var_b = most_recent_assignment(source, "out_attr0.z")?;
+
+    // Check if the RGB outputs are multiplied by alpha.
+    Some(
+        is_multiplied_by_alpha(source, var_r, multiplied_alpha_var)
+            && is_multiplied_by_alpha(source, var_g, multiplied_alpha_var)
+            && is_multiplied_by_alpha(source, var_b, multiplied_alpha_var),
+    )
+}
+
+fn is_multiplied_by_alpha(source: &str, var: &str, alpha_var: &str) -> bool {
+    if let Some(assignment) = most_recent_assignment(source, var) {
+        assignment.contains(&format!("* {alpha_var}"))
+            || assignment.contains(&format!("{alpha_var} *"))
+    } else {
+        false
+    }
+}
+
+fn most_recent_assignment<'a>(source: &'a str, var: &str) -> Option<&'a str> {
+    source
+        .lines()
+        .rfind(|l| l.contains(&format!("{var} = ")))
+        .and_then(|s| s.split_once("="))
+        .map(|(_, s)| s.trim().trim_end_matches(';'))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::is_premultiplied_alpha;
+
+    #[test]
+    fn is_premultiplied() {
+        let source = "temp_743 = fma(temp_736, temp_742, temp_736);
+            // 0x001850: 0x5C68100000570201 Fmul
+            temp_744 = temp_739 * temp_736;
+            // 0x001858: 0x5C68100000570000 Fmul
+            temp_745 = temp_740 * temp_736;
+            // 0x001868: 0x5C68100000570402 Fmul
+            temp_746 = temp_741 * temp_736;
+            // 0x001870: 0xE30000000007000F Exit
+            if (!s_is_bgra[0])
+            {
+                out_attr0.x = temp_745;
+                temp_747 = true;
+            }
+            else
+            {
+                out_attr0.z = temp_745;
+            }
+            temp_747 = false;
+            out_attr0.y = temp_744;
+            if (!s_is_bgra[0])
+            {
+                out_attr0.z = temp_746;
+                temp_748 = true;
+            }
+            else
+            {
+                out_attr0.x = temp_746;
+            }
+            temp_748 = false;
+            out_attr0.w = temp_743;";
+
+        assert!(is_premultiplied_alpha(source).unwrap_or_default());
+    }
+
+    #[test]
+    fn pixel_source_not_premultiplied() {
+        let source = "temp_733 = temp_730 * temp_680;
+            // 0x0017F8: 0x49A002AC06870000 Ffma
+            temp_734 = fma(temp_722, fp_c11_data[26].x, temp_732);
+            // 0x001808: 0x49A002AC06870401 Ffma
+            temp_735 = fma(temp_724, fp_c11_data[26].x, temp_732);
+            // 0x001810: 0x49A002AC06870202 Ffma
+            temp_736 = fma(temp_726, fp_c11_data[26].x, temp_732);
+            // 0x001818: 0xE30000000007000F Exit
+            if (!s_is_bgra[0])
+            {
+                out_attr0.x = temp_734;
+                temp_737 = true;
+            }
+            else
+            {
+                out_attr0.z = temp_734;
+            }
+            temp_737 = false;
+            out_attr0.y = temp_735;
+            if (!s_is_bgra[0])
+            {
+                out_attr0.z = temp_736;
+                temp_738 = true;
+            }
+            else
+            {
+                out_attr0.x = temp_736;
+            }
+            temp_738 = false;
+            out_attr0.w = temp_733;";
+
+        assert!(!is_premultiplied_alpha(source).unwrap_or_default());
+    }
+
+    #[test]
+    fn pixel_source_not_premultiplied_empty() {
+        assert!(!is_premultiplied_alpha("").unwrap_or_default());
+    }
 }
